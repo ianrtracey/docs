ca5f0bcb8c4e1f53acf982163d444e8fc1688d21
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-03-04 14:46:01 -0600

    Walkthrough start

--- scripts/preview.py
+++ scripts/preview.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     print(f"Watching directory '{root}' for changes...")
     try:
         while True:
-            time.sleep(0.2)
+            time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
         observer.join()
