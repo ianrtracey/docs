6815a702f6afacfd51a2bd70bd9052f37e3601fd
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-03-06 18:42:35 -0600

    showcase

--- scripts/rerun_jupyter.py
+++ scripts/rerun_jupyter.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     for root, dirs, files in os.walk(os.path.dirname(os.path.dirname(__file__))):
         for file in glob.glob(os.path.join(root, "*.ipynb")):
             subprocess.run(
-                f"jupyter nbconvert --to notebook --execute --inplace {file}",
+                f"jupyter nbconvert --to notebook --execute --clear-output {file}",
                 shell=True,
             )
